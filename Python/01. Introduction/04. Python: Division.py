6e01a27305a3d0a9ff0c4554f59f784b2b572605
Fiorella Martinez <127914908+fioremartiro@users.noreply.github.com>
2024-07-15 21:14:35 -0600

    Update 04. Python: Division.py

--- Python/01. Introduction/04. Python: Division.py
+++ Python/01. Introduction/04. Python: Division.py
@@ -1,1 +1,9 @@
+# Task
+# The provided code stub reads two integers,  and , from STDIN.
+# Add logic to print two lines. The first line should contain the result of integer division,  // . The second line should contain the result of float division,  / .
 
+int_division = a//b
+print(int_division)
+
+float_division = a / b
+print(float_division)
